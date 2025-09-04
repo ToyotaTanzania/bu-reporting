cb9117fb77ce3f6c933bdb8733ce722442a54998
hvrdt <eckhvrdt@gmail.com>
2025-09-04 09:49:28 +0300

    Refactor get_client_ip function to improve client IP retrieval logic based on GAE_ENV

--- dependencies.py
+++ dependencies.py
@@ -1,11 +1,13 @@
+import os
 from fastapi import Request
 
 
 def get_client_ip(request: Request) -> str:
-    forwarded = request.headers.get('X-Forwarded-For')
-    if forwarded:
-        client_ip = forwarded.split(',')[-1].strip()
+    if os.getenv('GAE_ENV', '').startswith('standard'):
+        forwarded_for = request.headers.get('X-Forwarded-For', '')
+        if forwarded_for:
+            return forwarded_for.split(',')[0].strip()
+        else:
+            return request.client.host if request.client else None
     else:
-        client_ip = request.client.host
-
-    return client_ip
+        return request.client.host if request.client else None
