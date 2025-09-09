fe14ffd1c08375ada2b2ab6b779cd1e941a95163
solointer11 <inter.solo11@yahoo.com>
2025-09-09 13:19:06 +0300

    config changes

--- database.py
+++ database.py
@@ -6,11 +6,7 @@ from config import settings, logger
 
 db_pool = Queue(maxsize=settings.DB_POOL_SIZE)
 
-def _create_connection():    
-    print('TESTING RON DB_SERVER', settings.DB_SERVER)
-    print('TESTING RON DB_USER', settings.DB_USER)
-    print('TESTING RON DB_PASSWORD', settings.DB_PASSWORD)
-    print('TESTING RON DB_DATABASE', settings.DB_DATABASE)
+def _create_connection():
     try:
         return pymssql.connect(
             server=settings.DB_SERVER,
